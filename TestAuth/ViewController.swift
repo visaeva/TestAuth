67bee3f6f33f566ce2969329f06b6ff857ab75ae
visaeva <visaeva0302@gmail.com>
2025-09-04 13:41:37 +0300

    Initial Commit

---
+++ TestAuth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestAuth
+//
+//  Created by Victoria Isaeva on 04.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
